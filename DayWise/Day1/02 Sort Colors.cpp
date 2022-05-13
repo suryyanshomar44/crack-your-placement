169b6781ba0142c1bad9be0092c089ab8064591d
Suryansh Omar <70739290+suryyanshomar44@users.noreply.github.com>
2022-05-13 21:37:28 +0530

    Create 02 Sort Colors.cpp
    Array

---
+++ DayWise/Day1/02 Sort Colors.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int lo = 0, hi = nums.size()-1, mid = 0;
+        while(mid<=hi) {
+            if(nums[mid] == 0) {
+                int temp = nums[lo];
+                nums[lo] = nums[mid];
+                nums[mid] = temp;
+                lo++;
+                mid++;
+            }else if(nums[mid] == 1) {
+                mid++;
+            }
+            else if(nums[mid] == 2) {
+                int temp = nums[hi];
+                nums[hi] = nums[mid];
+                nums[mid] = temp;
+                hi--;
+            }
+        }
+    }
+};
