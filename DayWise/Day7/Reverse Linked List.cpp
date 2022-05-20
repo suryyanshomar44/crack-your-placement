be6bbc0428f1ce1ff3f6d1c5d7f17af182d9bacb
Suryansh Omar <70739290+suryyanshomar44@users.noreply.github.com>
2022-05-20 20:02:46 +0530

    LinkedList

---
+++ DayWise/Day7/Reverse Linked List.cpp
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/reverse-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* temp = head;
+        while(temp!=NULL) {
+            ListNode* n = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = n;
+        }
+        return prev;
+    }
+};
